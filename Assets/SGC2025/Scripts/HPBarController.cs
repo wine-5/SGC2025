8bd790dcfd61e8e6ebdf6a21d5ed1947d57578d7
MSI\akani <akaniwas0202@gmail.com>
2025-10-19 13:01:50 +0900

    PlayerをPlayerCharacterに修正

--- Assets/SGC2025/Scripts/HPBarController.cs
+++ Assets/SGC2025/Scripts/HPBarController.cs
@@ -1,33 +1,46 @@
 using UnityEngine;
 
+using SGC2025.Enemy;
+
 namespace SGC2025
 {
     public class HPBarController : MonoBehaviour
     {
         [SerializeField] GameObject entity;
+        
+
         [Range(0f, 1f)]
         [SerializeField] private float rate;
         private float maxHealth;
         private float currentHealth;
+        private Vector3 originalPos;
         private Vector3 originalScale;
 
         [SerializeField] private bool isPlayer;
 
         void Start()
         {
+            originalPos = transform.parent.position;
             originalScale = transform.localScale;
 
             if (isPlayer)
             {
-                maxHealth = entity.GetComponent<Player>().GetPlayerMaxHealth();
+                maxHealth = entity.GetComponent<PlayerCharacter>().GetPlayerMaxHealth();
+
+            }
+            else
+            {
+                //maxHealth = 
             }
         }
 
         void Update()
         {
+            transform.parent.position = entity.transform.position + originalPos;
+
             if (isPlayer)
             {
-                 currentHealth = entity.GetComponent<Player>().GetPlayerCurrentHalth();
+                 currentHealth = entity.GetComponent<PlayerCharacter>().GetPlayerCurrentHalth();
             }
 
 
