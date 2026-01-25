fd67fcd46a5978366843e213f9e13463fbf9d1a8
YutoImata <yuto.imata.wine@gmail.com>
2026-01-25 14:18:21 +0900

    feat: プレイヤーがダメージを受けた時にカメラをシェイクする処理を追加

--- Assets/SGC2025/Scripts/InGame/Camera/CameraShake.cs
+++ Assets/SGC2025/Scripts/InGame/Camera/CameraShake.cs
@@ -2,18 +2,37 @@ using UnityEngine;
 
 namespace SGC2025
 {
-    public class CameraShake : MonoBehaviour
+    /// <summary>
+    /// カメラシェイクの設定とパラメータを管理するクラス
+    /// </summary>
+    [System.Serializable]
+    public class CameraShake
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [Header("シェイク設定")]
+        [SerializeField, Tooltip("シェイクの継続時間（秒）")]
+        private float duration = 0.2f;
         
-        }
+        [SerializeField, Tooltip("シェイクの強さ")]
+        private float magnitude = 0.1f;
+        
+        [SerializeField, Tooltip("低HP時のシェイク強度倍率")]
+        private float lowHpMultiplier = 2f;
+        
+        [SerializeField, Tooltip("低HPと判定するHP割合")]
+        [Range(0f, 1f)]
+        private float lowHpThreshold = 0.3f;
 
-        // Update is called once per frame
-        void Update()
+        public float Duration => duration;
+        public float Magnitude => magnitude;
+
+        /// <summary>
+        /// HP率に応じたシェイクの強さを取得
+        /// </summary>
+        public float GetMagnitudeByHpRate(float hpRate)
         {
-        
+            if (hpRate <= lowHpThreshold)
+                return magnitude * lowHpMultiplier;
+            return magnitude;
         }
     }
 }
