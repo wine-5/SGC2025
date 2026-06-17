ac651aaa56725add59cf83cccc4e86976d107e95
motokjy <kjtmtk@icloud.com>
2026-06-18 01:25:20 +0900

    GreeningEffectを修正

---
+++ Assets/SGC2025/Scripts/Effect/GreeningController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace SGC2025
+{
+    public class GreeningController : MonoBehaviour
+    {
+        [SerializeField] private VisualEffect greeningEffect;
+        [SerializeField] private Vector2 gaugeViewportPoint = new Vector2(0.08f, 0.88f);
+
+        void Update()
+        {
+            greeningEffect.SetVector3("UIPos",ResolveWorldPosition());
+        }
+
+        private Vector3 ResolveWorldPosition()
+        {
+            UnityEngine.Camera cam = UnityEngine.Camera.main;
+
+            if (cam != null)
+            {
+                Vector3 viewport = new Vector3(gaugeViewportPoint.x, gaugeViewportPoint.y, Mathf.Abs(cam.transform.position.z));
+                Vector3 world = cam.ViewportToWorldPoint(viewport);
+                world.z = 0f;
+                return world;
+            }
+
+            return new (0,0,0);
+        }
+    }
+}
